be91bd872118b91aafbc2180e6cecb410c84c87b
小左同学 <zuogl448@163.com>
2024-12-27 11:28:03 +0800

    Initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,21 @@
+export default {
+  title: "我的网站",
+  description: "使用 VitePress 构建的网站",
+  
+  // 如果部署到 GitHub Pages，需要设置 base
+  // 格式: /<仓库名>/
+  base: '/ZuoglTools/',
+
+  themeConfig: {
+    // 导航栏
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '指南', link: '/guide/' }
+    ],
+
+    // 社交链接
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/zuogl/ZuoglTools' }
+    ]
+  }
+} 
---
+++ docs/index.md
@@ -0,0 +1,14 @@
+---
+layout: home
+hero:
+  name: 我的网站
+  text: 使用 VitePress 构建
+  tagline: 简单、强大、高性能的静态网站
+  actions:
+    - theme: brand
+      text: 开始使用
+      link: /guide/
+    - theme: alt
+      text: GitHub
+      link: https://github.com/<YOUR_NAME>/<REPO_NAME>
+---
